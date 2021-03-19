95229d4cf5e28b02507ca446665e71b1250837b3
Kateryna Pryshchenko <longandlonesome@gmail.com>
2021-03-19 22:40:44 +0200

    project1

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -15,11 +15,13 @@ const Accordion = () => {
     return (
         <IconContext.Provider value={{color: 'hsl(14, 88%, 65%)', size: '25px'}}>
             {Data.map((item, index) => {
+                let classes = 'title'
+                if (clicked === index) classes += ' selected'
                 return (
                     <div className="item">
-                        <div onClick={() => toggleClicked(index)} key={index} className="title">
+                        <div onClick={() => toggleClicked(index)} key={index} className={classes}>
                             <h2>{item.question}</h2>
-                            <span>{clicked === index ? <FiChevronUp className="chevron"/> : <FiChevronDown className="chevron"/>}</span>
+                            <span>{clicked === index ? <FiChevronUp /> : <FiChevronDown />}</span>
                         </div>
                         {clicked === index ? (
                             <div className="dropdown">
